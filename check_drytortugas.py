ba8e6c49f064b43ef9f4d87e8db629fd7091afd9
anntedd <annmarie.tedesco@gmail.com>
2025-12-15 15:50:40 -0600

    Update check_drytortugas.py

--- check_drytortugas.py
+++ check_drytortugas.py
@@ -7,12 +7,12 @@ EMAIL_TO = os.environ["EMAIL_TO"]
 EMAIL_PASSWORD = os.environ["EMAIL_PASSWORD"]
 
 msg = MIMEText("If you got this, GitHub Actions can email you.")
-msg["Subject"] = "✅ Dry Tortugas Email Test"
+msg["Subject"] = "Dry Tortugas Email Test"
 msg["From"] = EMAIL_FROM
 msg["To"] = EMAIL_TO
 
-with smtplib.SMTP("smtp.gmail.com", 587) as server:
-    server.starttls()
+# Use SSL directly
+with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
     server.login(EMAIL_FROM, EMAIL_PASSWORD)
     server.send_message(msg)
 
